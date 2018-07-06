8e19d99937fa819ef102e91b83ac7dfa3f1918b6
Henrique Lazzarini <henlazza@hotmail.com>
2018-07-05 21:30:12 -0300

    APP01 - Concluído.

---
+++ Seção05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Serviço.Modelo;
+using App01_ConsultarCEP.Serviço;
+
+
+namespace App01_ConsultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Lógica do Programa
+
+
+            
+            //TODO - Validações 
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {2}, {3}, {0} - {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "O CEP: " + cep + " não existe", "OK");
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("Erro Crítico", e.Message, "OK");
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            Boolean valido = true;
+
+            if(cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP inválido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+
+            int novoCEP = 0;
+            if(!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("Erro", "CEP inválido! O CEP deve conter apenas números", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+	}
+}
---
+++ Seção05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Serviço/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Serviço.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Serviço
+{
+    class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            String Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
